a1cf2290d5fcf453f124a618719c9369e6a6bd07
Manas Kishore <manaskishoredev@gmail.com>
2025-02-24 22:35:49 +0530

    Learning Loops In Lua

--- main.lua
+++ main.lua
@@ -1,14 +1,6 @@
-local msg = false
+Message = 0
 
-print("Enter A Number below 10 to get true: ")
-Condition = io.read("n");
-
-if Condition < 10 then
-    msg = true
-elseif Condition > 20 then
-    msg = true
-else
-    msg = false
+while Message <= 1000000 do
+    Message = Message + 1
+    print(Message)
 end
-
-print(msg)
