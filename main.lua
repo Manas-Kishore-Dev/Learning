ca62ac8a59ff3c1d38456d64a49ca74ee1037728
Manas Kishore <manaskishoredev@gmail.com>
2025-02-25 12:44:20 +0530

    Learning Lua Love2d

--- main.lua
+++ main.lua
@@ -1,6 +1,14 @@
-Array = {"Banana", "Apple", "Orange", "Grape", "Pear"}
+local love = require("love")
+function love.load()
+    Player = {}
 
-
-for i,s in ipairs(Array) do
-    print(s)
+    Player.x = 400
+    Player.y = 200
 end
+
+function love.update(dt)
+end
+
+function love.draw ()
+    love.graphics.circle("fill", Player.x, Player.y, 100)
+end 
