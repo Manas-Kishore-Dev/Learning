af355defb70bd088e8c7ed7ee5acbc1561ab407b
Manas Kishore <manaskishoredev@gmail.com>
2025-02-28 14:51:33 +0530

    Relearning from docs

--- conf.lua
+++ conf.lua
@@ -1,7 +1,8 @@
 _G.love = require "love"
 
 function love.conf(t)
-    t.window.title = "Minesweeper"
+    t.window.title = "Flappy Bird Clone"
     t.window.width = 765
     t.window.height = 765
+    t.version = "b1.01.11"
 end
